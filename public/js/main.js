488e124184b0d35c9a237fb7962d6ac270c31445
huskycodez <jk.temp2018@gmail.com>
2021-06-21 14:49:49 +0800

    fix

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function delay(n) {
     n = n || 2000;
     return new Promise((done) => {
@@ -57,63 +56,3 @@ $(function () {
         ],
     });
 });
-=======
-function delay(n) {
-    n = n || 2000;
-    return new Promise((done) => {
-        setTimeout(() => {
-            done();
-        }, n);
-    });
-}
-
-function pageTransition() {
-    var tl = gsap.timeline();
-    tl.to(".loading-screen", {
-        duration: 1.2,
-        width: "100%",
-        left: "0%",
-        ease: "Expo.easeInOut",
-    });
-
-    tl.to(".loading-screen", {
-        duration: 1,
-        width: "100%",
-        left: "100%",
-        ease: "Expo.easeInOut",
-        delay: 0.1,
-    });
-    tl.set(".loading-screen", { left: "-100%" });
-}
-
-function contentAnimation() {
-    var tl = gsap.timeline();
-    tl.from(".animate-this", { duration: 1, y: 30, opacity: 0, stagger: 0.4, delay: 0.2 });
-}
-
-$(function () {
-    barba.init({
-        sync: true,
-
-        transitions: [
-            {
-                async leave(data) {
-                    const done = this.async();
-
-                    pageTransition();
-                    await delay(1000);
-                    done();
-                },
-
-                async enter(data) {
-                    contentAnimation();
-                },
-
-                async once(data) {
-                    contentAnimation();
-                },
-            },
-        ],
-    });
-});
->>>>>>> ae1b782f38a537a97ca4608335bfbbc5737f1086
